021001b10350a4aa03f2d14a8212cb0e882acf7b
Clodus-NT <clodus.nt@gmail.com>
2022-08-18 11:17:19 -0400

    added a projects link to splash page and changes some styling

--- src/components/main-view/MainView.js
+++ src/components/main-view/MainView.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./MainView.css"
 
 export default function MainView(props) {
   return (
-    <Container>
+    <Container className="mainContainer">
       <div className="splashContainer">
-        <p className="splashText">Hi! I'm a web developer based in Tallahassee, Florida.</p>
-        {/* <div className="btnContainer">
-          <Button>Resume</Button>
-          <Button>Github</Button>
-        </div> */}
+        <p className="splashText">
+          Hi! I'm Nick, a web developer based in Tallahassee, Florida. Check out some of my <a  className="projectsLink" href="/projects">projects</a>.
+        </p>
       </div>
     </Container>
   )
