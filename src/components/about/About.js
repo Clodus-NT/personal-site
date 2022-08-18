88479c6f91ac5c28f72c8ed565f13669df5795fe
Clodus-NT <clodus.nt@gmail.com>
2022-08-18 10:30:59 -0400

    fixed a typo in About.js blurb

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,7 @@ export default function About() {
       <div className="blurb-container">
         <h1 className="blurb-header">About Me</h1>
         <p className="blurb-text">
-          Hello World! I'm Nick, a web developer currently residing in Tallahassee, Florida. I discovered a love for coding after deciding to move on from retail work and was pleasanlty surprised to find that the skills I had learned such as time management, flexibility, and problem solving carried over into web development. My passion for coding is occompanied by a desire to never stop learning.
+          Hello World! I'm Nick, a web developer currently residing in Tallahassee, Florida. I discovered a love for coding after deciding to move on from retail work and was pleasanlty surprised to find that the skills I had learned such as time management, flexibility, and problem solving carried over into web development. My passion for coding is accompanied by a desire to never stop learning.
         </p>
         <p className="socials-text">Check out my socials or download my resume:</p>
         <div className="socials-container">
