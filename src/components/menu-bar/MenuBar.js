1515516e99920419a97f156f23b26324008f9119
Clodus-NT <clodus.nt@gmail.com>
2022-08-18 10:04:34 -0400

    added padding-left to navbar

--- src/components/menu-bar/MenuBar.js
+++ src/components/menu-bar/MenuBar.js
@@ -1,21 +1,16 @@
 import React from "react";
 import './menu-bar.css';
-import { Navbar, Container, Nav, } from 'react-bootstrap';
+import { Navbar, Nav, } from 'react-bootstrap';
 
 export default function MenuBar() {
   return (
     <Navbar className="nav navColors" variant='navColors'>
-      {/* <Container className="navContainer navColors"> */}
-        <Navbar.Brand  className='navColors navBrand' href='/'>Nick Turner</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav navbarScroll' />
-        {/* <Navbar.Collapse className='navColors' id="responsive-navbar-nav"> */}
-          <Nav className='navColors navLinkContainer'>
-            <Nav.Link className='navColors navLink' href="/">Home</Nav.Link>
-            <Nav.Link className='navColors navLink' href="/projects">Projects</Nav.Link>
-            <Nav.Link className='navColors navLink' href="/about">About</Nav.Link>
-          </Nav>
-        {/* </Navbar.Collapse> */}
-      {/* </Container> */}
+      <Navbar.Brand  className='navColors navBrand' href='/'>Nick Turner</Navbar.Brand>
+      <Nav className='navColors navLinkContainer'>
+        <Nav.Link className='navColors navLink' href="/">Home</Nav.Link>
+        <Nav.Link className='navColors navLink' href="/projects">Projects</Nav.Link>
+        <Nav.Link className='navColors navLink' href="/about">About</Nav.Link>
+      </Nav>
     </Navbar>
   )
 }
