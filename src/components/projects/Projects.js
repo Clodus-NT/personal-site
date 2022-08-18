5b3c782013f4c3a1beefd0484a8b4e8783e1e8de
Clodus-NT <clodus.nt@gmail.com>
2022-08-18 11:00:02 -0400

    changed class to className in Footer component // moved Card.Img to be below the Card.Title

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -12,16 +12,18 @@ export default function Projects() {
         <Col>
           <CardGroup className="bgColor cardGroup">
             <Card className="bgColor">
-                <Card.Body className="bgColor">
-                  <Card.Img className="bgColor" src='../../public/assets/pokedex.png' />
+                <Card.Body className="bgColor cardBody">
                   <Card.Title className="bgColor">JS-Dex</Card.Title>
+                  <Card.Img className="bgColor" src='../../public/assets/pokedex.png' />
                   <Card.Text className="bgColor">This pokedex contains all 151 original, generation 1 Pokemon. It loads data from an API and displays each pokemon's name on a button that opens up a bootstrap modal displaying corresponding stats.</Card.Text>
-                  <Card.Link className="bgColor project-link" href='https://github.com/Clodus-NT/js-dex' target='blank' >
-                    <Button className="linkBtn"  >Code</Button>
-                  </Card.Link>
-                  <Card.Link className="bgColor project-link" href='https://clodus-nt.github.io/js-dex/' target='blank' >
-                    <Button className="linkBtn" >Live</Button>
-                  </Card.Link>
+                  <div className="bgColor">
+                    <Card.Link className="bgColor project-link" href='https://github.com/Clodus-NT/js-dex' target='blank' >
+                      <Button className="linkBtn"  >Code</Button>
+                    </Card.Link>
+                    <Card.Link className="bgColor project-link" href='https://clodus-nt.github.io/js-dex/' target='blank' >
+                      <Button className="linkBtn" >Live</Button>
+                    </Card.Link>
+                  </div>
               </Card.Body>
             </Card>
           </CardGroup>
@@ -31,13 +33,16 @@ export default function Projects() {
         <Col>
           <CardGroup className="bgColor cardGroup">
             <Card className="bgColor">
-                <Card.Body className="bgColor">
+                <Card.Body className="bgColor cardBody">
                   <Card.Title className="bgColor">Chat App</Card.Title>
+                  <Card.Img className="bgColor" src='../../public/assets/pokedex.png' />
                   <Card.Text className="bgColor">A simple chat app for mobile devices using React Native. The app provides users with a chat interface, the option to share images and the option to share location.</Card.Text>
                   <Card.Img className="bgColor" href='./project-imgs/pokedex.png' />
-                  <Card.Link className="bgColor" href='https://github.com/Clodus-NT/chat-app' target='blank' >
-                    <Button className="linkBtn"  >Code</Button>
-                  </Card.Link>
+                  <div className="bgColor">
+                    <Card.Link className="bgColor" href='https://github.com/Clodus-NT/chat-app' target='blank' >
+                      <Button className="linkBtn"  >Code</Button>
+                    </Card.Link>
+                  </div>
               </Card.Body>
             </Card>
           </CardGroup>
@@ -47,12 +52,14 @@ export default function Projects() {
         <Col>
           <CardGroup className="bgColor cardGroup">
             <Card className="bgColor card">
-                <Card.Body className="bgColor">
-                  <Card.Title className="bgColor">Movie API</Card.Title>
-                  <Card.Text className="bgColor">The server side component for my nixFlix web application. It provides access to movie and user info.</Card.Text>
+              <Card.Body className="bgColor cardBody">
+                <Card.Title className="bgColor">Movie API</Card.Title>
+                <Card.Text className="bgColor">The server side component for my nixFlix web application. It provides access to movie and user info.</Card.Text>
+                <div className="bgColor">
                   <Card.Link className="bgColor" href='https://github.com/Clodus-NT/movie-api' target='blank' >
                     <Button className="linkBtn"  >Code</Button>
                   </Card.Link>
+                </div>
               </Card.Body>
             </Card>
           </CardGroup>
@@ -62,16 +69,18 @@ export default function Projects() {
         <Col>
           <CardGroup className="bgColor cardGroup">
             <Card className="bgColor">
-                <Card.Body className="bgColor">
-                  <Card.Img className="bgColor" variant='top' src='../../public/assets/pokedex.png' />
-                  <Card.Title className="bgColor">nixFlix Move App</Card.Title>
-                  <Card.Text className="bgColor">A client-side movie application created with React.js that is based on existing server-side code (REST API and database).</Card.Text>
+              <Card.Body className="bgColor cardBody">
+                <Card.Title className="bgColor">nixFlix Move App</Card.Title>
+                <Card.Img className="bgColor" src='../../public/assets/pokedex.png' />
+                <Card.Text className="bgColor">A client-side movie application created with React.js that is based on existing server-side code (REST API and database).</Card.Text>
+                <div className="bgColor">
                   <Card.Link className="bgColor project-link" href='https://github.com/Clodus-NT/nix-flix-client' target='blank' >
                     <Button className="linkBtn" >Code</Button>
                   </Card.Link>
                   <Card.Link className="bgColor project-link" href='https://clodus-nt.github.io/js-dex/' target='blank' >
                     <Button className="linkBtn" >Live</Button>
                   </Card.Link>
+                </div>
               </Card.Body>
             </Card>
           </CardGroup>
@@ -81,16 +90,18 @@ export default function Projects() {
         <Col>
           <CardGroup className="bgColor cardGroup">
             <Card className="bgColor">
-                <Card.Body className="bgColor">
-                  <Card.Img className="bgColor" variant='top' src='../../public/assets/pokedex.png' />
+                <Card.Body className="bgColor cardBody">
                   <Card.Title className="bgColor">To-Do App</Card.Title>
+                  <Card.Img className="bgColor" src='../../public/assets/pokedex.png' />
                   <Card.Text className="bgColor">A simple To-Do application that allows you to add items, cross them out, remove them, and reorder them.</Card.Text>
-                  <Card.Link className="bgColor project-link" href='https://github.com/Clodus-NT/to-do-app' target='blank' >
-                    <Button className="linkBtn" >Code</Button>
-                  </Card.Link>
-                  <Card.Link className="bgColor project-link" href='https://clodus-nt.github.io/to-do-app/' target='blank' >
-                    <Button className="linkBtn" >Live</Button>
-                  </Card.Link>
+                  <div className="bgColor">
+                    <Card.Link className="bgColor project-link" href='https://github.com/Clodus-NT/to-do-app' target='blank' >
+                      <Button className="linkBtn" >Code</Button>
+                    </Card.Link>
+                    <Card.Link className="bgColor project-link" href='https://clodus-nt.github.io/to-do-app/' target='blank' >
+                      <Button className="linkBtn" >Live</Button>
+                    </Card.Link>
+                  </div>
               </Card.Body>
             </Card>
           </CardGroup>
