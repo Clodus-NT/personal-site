25b1ccdc52e8f2379b33aa8c97bafa84be7632ed
Clodus-NT <clodus.nt@gmail.com>
2022-08-24 15:00:08 -0400

    more projects section styling changes

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,24 +1,25 @@
 import React from "react";
-import {Button, Card, CardGroup, Container, Row, Col} from 'react-bootstrap';
+import {Button, Card, CardGroup, Container, Row} from 'react-bootstrap';
 import './projects.css';
 
 export default function Projects() {
   return (
     <Container fluid className="projectsContainer">
-      <Col className="projectsHeaders">
+      {/* <Col className="projectsHeaders">
         <h1>Projects</h1>
         <p className="projectsHeader2">Check out some of my projects!</p>
-      </Col>
+      </Col> */}
 
       <Row className="cardsContainer">
+
         {/* Pokedex */}
           <CardGroup className="bgColor pokedexCard cardGroup">
             <Card className="bgColor cardElement">
                 <Card.Body className="bgColor cardBody">
                   <Card.Title className="bgColor">JS-Dex</Card.Title>
                   <div className="imageContainer bgColor">
-                    <Card.Img className="cardImage" src={require ('./img/pokedex.png')} />
-                    <Card.Img className="cardImage" src={require ('./img/gengar.png')} />
+                    <Card.Img className="cardImage pokedexImageMain" src={require ('./img/pokedex.png')} />
+                    <Card.Img className="cardImage pokedexImageStats" src={require ('./img/gengar.png')} />
                   </div>
                   <Card.Text className="bgColor">This pokedex contains all 151 original, generation 1 Pokemon. It loads data from an API and displays each pokemon's name on a button that opens up a bootstrap modal displaying corresponding stats.</Card.Text>
                   <div className="bgColor">
@@ -34,7 +35,7 @@ export default function Projects() {
           </CardGroup>
 
         {/* nixFlix */}
-          <CardGroup className="bgColor apiCard cardGroup">
+          <CardGroup className="bgColor movieAppCard cardGroup">
             <Card className="bgColor cardElement">
               <Card.Body className="bgColor cardBody">
                 <Card.Title className="bgColor">nixFlix Movie App</Card.Title>
@@ -57,15 +58,35 @@ export default function Projects() {
             <Card className="bgColor cardElement">
                 <Card.Body className="bgColor cardBody">
                   <Card.Title className="bgColor">Chat App</Card.Title>
-                  {/* <Card.Img className="cardImage" src={require ('./img/chatapp1.png')} /> */}
-                  <Card.Img className="cardImage" src={require ('./img/chatapp2.png')} />
+                  <div className="imageContainer bgColor">
+                    <Card.Img className="cardImage" src={require ('./img/chatapp1.png')} />
+                    <Card.Img className="cardImage chatAppImage" src={require ('./img/chatapp2.png')} />
+                  </div>
                   <Card.Text className="bgColor">A simple chat app for mobile devices using React Native. The app provides users with a chat interface, the option to share images and the option to share location.</Card.Text>
-                  <Card.Img className="bgColor" href='./project-imgs/pokedex.png' />
                   <div className="bgColor">
                     <Card.Link className="bgColor" href='https://github.com/Clodus-NT/chat-app' target='blank' >
                       <Button className="linkBtn"  >Code</Button>
                     </Card.Link>
                   </div>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+
+        {/* To-Do App */}
+          <CardGroup className="bgColor todoCard cardGroup">
+            <Card className="bgColor cardElement">
+              <Card.Body className="bgColor cardBody">
+                <Card.Title className="bgColor">To-Do App</Card.Title>
+                <Card.Img className="cardImage todoImage" src={require ('./img/todo.png')} />
+                <Card.Text className="bgColor">A simple To-Do application that allows you to add items, cross them out, remove them, and reorder them.</Card.Text>
+                <div className="bgColor">
+                  <Card.Link className="bgColor project-link" href='https://github.com/Clodus-NT/to-do-app' target='blank' >
+                    <Button className="linkBtn" >Code</Button>
+                  </Card.Link>
+                  <Card.Link className="bgColor project-link" href='https://clodus-nt.github.io/to-do-app/' target='blank' >
+                    <Button className="linkBtn" >Live</Button>
+                  </Card.Link>
+                </div>
               </Card.Body>
             </Card>
           </CardGroup>
@@ -85,24 +106,6 @@ export default function Projects() {
             </Card>
           </CardGroup>
 
-        {/* To-Do App */}
-          <CardGroup className="bgColor apiCard cardGroup">
-            <Card className="bgColor cardElement">
-                <Card.Body className="bgColor cardBody">
-                  <Card.Title className="bgColor">To-Do App</Card.Title>
-                  <Card.Img className="cardImage" src={require ('./img/todo.png')} />
-                  <Card.Text className="bgColor">A simple To-Do application that allows you to add items, cross them out, remove them, and reorder them.</Card.Text>
-                  <div className="bgColor">
-                    <Card.Link className="bgColor project-link" href='https://github.com/Clodus-NT/to-do-app' target='blank' >
-                      <Button className="linkBtn" >Code</Button>
-                    </Card.Link>
-                    <Card.Link className="bgColor project-link" href='https://clodus-nt.github.io/to-do-app/' target='blank' >
-                      <Button className="linkBtn" >Live</Button>
-                    </Card.Link>
-                  </div>
-              </Card.Body>
-            </Card>
-          </CardGroup>
       </Row>
     </Container>
   )
