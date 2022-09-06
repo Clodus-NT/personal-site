c67467316910ef620d87368225842bc62372c248
Clodus-NT <clodus.nt@gmail.com>
2022-09-06 14:30:45 -0400

    commented out <Nav> as it was causing styling issues and function without

--- src/components/menu-bar/MenuBar.js
+++ src/components/menu-bar/MenuBar.js
@@ -6,12 +6,12 @@ export default function MenuBar() {
   return (
     <Navbar className="nav navColors" variant='navColors'>
       <Navbar.Brand  className='navColors navBrand navName' href='#/'>Nick Turner</Navbar.Brand>
-      <Nav className='navColors navLinkContainer'>
+      {/* <Nav className='navColors navLinkContainer'> */}
         <Nav.Link className='navColors navLink navHome' href="#/">Home</Nav.Link>
         <Nav.Link className='navColors navLink navProjects' href="#/projects">Projects</Nav.Link>
         <Nav.Link className='navColors navLink navAbout' href="#/about">About</Nav.Link>
         <Nav.Link className='navColors navLink navAbout' href="#/contact">Contact</Nav.Link>
-      </Nav>
+      {/* </Nav> */}
     </Navbar>
   )
 }
